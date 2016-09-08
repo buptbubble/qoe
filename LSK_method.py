313af96d6bea0c1ab1aa91ab107550da6ed277cf
bubble <buptliyuchen@163.com>
2016-09-08 17:04:51 +0800

    906

--- LSK_method.py
+++ LSK_method.py
@@ -4,39 +4,29 @@ import numpy as np
 from numpy import linalg as la
 
 
-
-def windowPic(img,x,y,sizex,sizey):
-    return img[y:y+sizey,x:x+sizex]
+def copyBlock_center(img,center,size):
+    if size%2 != 1:
+        raise NameError('Image block size error')
+    x = center[0]
+    y = center[1]
+    r = (size-1)/2
+    upper = x-r
+    bottom = x+r+1
+    left = y-r
+    right = y+r+1
+    if upper<0:
+        upper=0
+    if bottom>img.shape[0]:
+        bottom=img.shape[0]
+    if left<0:
+        left=0
+    if right>img.shape[1]:
+        right=img.shape[1]
+    return img[upper:bottom,left:right]
 
 def mat2array(img):
     img_size = img.shape[0] * img.shape[1]
     return img.reshape(img_size,1)
-
-def LSK(center_x, cur_x,C,h):
-
-    det_c = la.det(mat_c)
-
-    return (np.sqrt(det_c)/(h**2))*np.exp((np.matrix(center_x-cur_x)*C*np.matrix(center_x-cur_x).T)/(-2 * h**2))
-
-def get_mat_C(roi_x,roi_y,pic_ori):
-    pic_deri_y = cv2.Sobel(pic_ori, cv2.CV_32F, 0, 1)
-    pic_deri_x = cv2.Sobel(pic_ori, cv2.CV_32F, 1, 0)
-    roipic_derix = windowPic(pic_deri_x, roi_x, roi_y, 5, 5)
-    roipic_deriy = windowPic(pic_deri_y, roi_x, roi_y, 5, 5)
-    roixarr = mat2array(roipic_derix)
-    roiyarr = mat2array(roipic_deriy)
-    mat_j = np.hstack((roixarr, roiyarr))
-    u, sigma, v = la.svd(mat_j)
-    s1 = sigma[0]
-    s2 = sigma[1]
-    v1 = np.matrix(v[:, 0])
-    v2 = np.matrix(v[:, 1])
-    a1 = (s1 + 1) / (s2 + 1)
-    a2 = (s2 + 1) / (s1 + 1)
-    gamma = np.power((s1 + s2 + 1e-6) / 9, 0.008)
-
-    mat_c = gamma * a1 ** a1 * v1.T * v1 + a2 ** a2 * v2.T * v2
-    return mat_c
 
 def get_surround_pixel(center_p):
     center_x = center_p[0]
@@ -55,43 +45,77 @@ def get_surround_pixel(center_p):
                 continue
 
 
-if __name__ == "__main__":
-    pics = cv2.imread('pics/Lenna.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    roi_x = 50
-    roi_y = 60
+class LSK_method:
+    def __init__(self,imgpath):
+        self.pic = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        self.pic_deri_x =  cv2.Sobel(self.pic, cv2.CV_32F, 1, 0)
+        self.pic_deri_y =  cv2.Sobel(self.pic, cv2.CV_32F, 0, 1)
+        self.h =1
 
-    mat_c = get_mat_C(roi_x,roi_y,pics)
+    def get_mat_C(self,center_p):
+        roi_x = center_p[0]
+        roi_y = center_p[1]
+        block_deri_x = copyBlock_center(self.pic_deri_x,(roi_x,roi_y),5)
+        block_deri_y = copyBlock_center(self.pic_deri_y,(roi_x,roi_y),5)
+        roixarr = mat2array(block_deri_x)
+        roiyarr = mat2array(block_deri_y)
+        mat_j = np.hstack((roixarr, roiyarr))
+        u, sigma, v = la.svd(mat_j)
+        s1 = sigma[0]
+        s2 = sigma[1]
+        v1 = np.matrix(v[:, 0])
+        v2 = np.matrix(v[:, 1])
+        a1 = (s1 + 1) / (s2 + 1)
+        a2 = (s2 + 1) / (s1 + 1)
+        gamma = np.power((s1 + s2 + 1e-6) / 9, 0.008)
+
+        mat_c = gamma * a1 ** a1 * v1.T * v1 + a2 ** a2 * v2.T * v2
+        return mat_c
+
+    def get_K(self,center_p,cur_p):
+        C = np.matrix(self.get_mat_C(center_p))
+        #print C
+        det_c = la.det(C)
+        #print det_c
+        return (np.sqrt(det_c) / (self.h ** 2)) * np.exp\
+            ((np.matrix(center_p - cur_p) * C * np.matrix(center_p - cur_p).T) / (-2 * self.h ** 2))
+
+    def get_W(self,center_p,cur_p):
+        K_surround = 0
+        for surround_p in get_surround_pixel(center_p):
+            #K_surround += self.get_K(surround_p,cur_p)
+            K_surround += self.get_K(surround_p,cur_p)
+
+        K_center = self.get_K(center_p, cur_p)
+        W_cur = K_center / K_surround
+        return W_cur
+
+    def get_W_img(self,center_p,size):
+        w_img = np.zeros((size,size))
+
+        if size%2 != 1:
+            raise NameError('size must be odder')
+        filter_r = (size-1)/2
+        for cur_x in range(size):
+            cur_x = cur_x - filter_r
+            for cur_y in range(size):
+                cur_y = cur_y - filter_r
+                delta_p = np.array([cur_x,cur_y])
+
+                cur_p = center_p + delta_p
+                W = self.get_W(center_p, cur_p)
+                w_img[cur_x + filter_r][cur_y + filter_r] = W
+        return w_img
+
+
+if __name__ == "__main__":
+    lsk = LSK_method('pics/Lenna.png')
+
     center_x = 50
     center_y = 50
-    filter_r = 5
-    h=1
-    center_arr = np.array([center_x,center_y])
+    center_p = np.array([center_x,center_y])
 
 
-    lsk_filter = np.zeros((filter_r*2,filter_r*2))
-
-    for cur_x in range(filter_r*2):
-        cur_x = cur_x-filter_r
-        for cur_y in range(filter_r*2):
-            cur_y = cur_y-filter_r
-
-            center_p = np.array([center_x,center_y])
-            cur_p = np.array([center_x+cur_x,center_y+cur_y])
-            K_surround = 0
-            for surround_p in get_surround_pixel(center_p):
-                K_surround += LSK(cur_p,surround_p,mat_c,h)
-            K_center = LSK(center_p,cur_p,mat_c,h)
-            W_cur = K_center/K_surround
-            lsk_filter[cur_x+filter_r][cur_y+filter_r] = W_cur
-    plt.imshow(lsk_filter,cmap='gray',interpolation='none')
+    wimg = lsk.get_W_img(center_p,11)
+    plt.imshow(wimg, cmap='gray', interpolation='none')
     plt.show()
-
-
-
-
-    #LSK()
-
-    print 'mat_c:\n',mat_c
-
-
-    exit(0)
--- main_window.py
+++ main_window.py
@@ -4,12 +4,16 @@ from Tkinter import *
 import cv2
 from PIL import Image
 from PIL import ImageTk
+from LSK_method import LSK_method
+import numpy as np
 
 class mainWindow():
     def __init__(self):
         self.root = Tk()
         self.tk_child = None
+        self.picpath = 'pics/Lenna.png'
 
+        self.runfunc()
     def mat2tkimg(self,matimg):
         return ImageTk.PhotoImage(Image.fromarray(matimg))
 
@@ -17,14 +21,22 @@ class mainWindow():
         print("Click position:", event.x, event.y)
         clickx = event.x
         clicky = event.y
+        clickp = np.array([event.x,event.y])
         if self.tk_child == None:
             self.tk_child = Toplevel(self.root,borderwidth=5)
             self.tk_child.attributes("-toolwindow",True)
             self.subtk_label1 = Label(master=self.tk_child)
             self.subtk_label1.pack()
-        mat_selected = self.pic[clicky:clicky+20,clickx:clickx+20]
-        self.image_sel = self.mat2tkimg(mat_selected)
-        self.subtk_label1.configure(image= self.image_sel)
+
+        w_img = self.lsk.get_W_img(clickp,11)
+
+        #mat_selected = self.pic[clicky:clicky+20,clickx:clickx+20]
+        #self.image_sel = self.mat2tkimg(mat_selected)
+
+
+        self.weight_img = self.mat2tkimg(w_img)
+        self.subtk_label1.configure(image=self.weight_img)
+
         self.tk_child.focus()
 
     def quit(self):
@@ -32,7 +44,9 @@ class mainWindow():
         self.root.destroy()
 
     def runfunc(self):
-        self.pic = cv2.imread('pics/Lenna.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        self.pic = cv2.imread(self.picpath,cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        self.lsk = LSK_method(self.picpath)
+
         image = Image.fromarray(self.pic)
         image = ImageTk.PhotoImage(image)
         label1 = Label(self.root,image = image)
@@ -43,4 +57,4 @@ class mainWindow():
 
 if __name__ == '__main__':
     mainWin = mainWindow()
-    mainWin.runfunc()
+
