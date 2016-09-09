7f5f5d31834ff145ff7690ca27abb8b3ac2bb544
bubble <buptliyuchen@163.com>
2016-09-09 17:47:38 +0800

    906

--- LSK_method.py
+++ LSK_method.py
@@ -48,8 +48,8 @@ def get_surround_pixel(center_p):
 class LSK_method:
     def __init__(self,imgpath):
         self.pic = cv2.imread(imgpath,cv2.CV_LOAD_IMAGE_GRAYSCALE)
-        self.pic_deri_x =  cv2.Sobel(self.pic, cv2.CV_32F, 1, 0)
-        self.pic_deri_y =  cv2.Sobel(self.pic, cv2.CV_32F, 0, 1)
+        self.pic_deri_x =  cv2.Sobel(self.pic, cv2.CV_32F, 1, 0)/8
+        self.pic_deri_y =  cv2.Sobel(self.pic, cv2.CV_32F, 0, 1)/8
         self.h =1
 
     def get_mat_C(self,center_p):
@@ -69,14 +69,12 @@ class LSK_method:
         a2 = (s2 + 1) / (s1 + 1)
         gamma = np.power((s1 + s2 + 1e-6) / 9, 0.008)
 
-        mat_c = gamma * a1 ** a1 * v1.T * v1 + a2 ** a2 * v2.T * v2
+        mat_c = gamma * ((a1 ** 2) * v1.T * v1 + (a2 ** 2) * v2.T * v2)
         return mat_c
 
     def get_K(self,center_p,cur_p):
         C = np.matrix(self.get_mat_C(center_p))
-        #print C
         det_c = la.det(C)
-        #print det_c
         return (np.sqrt(det_c) / (self.h ** 2)) * np.exp\
             ((np.matrix(center_p - cur_p) * C * np.matrix(center_p - cur_p).T) / (-2 * self.h ** 2))
 
@@ -89,6 +87,23 @@ class LSK_method:
         K_center = self.get_K(center_p, cur_p)
         W_cur = K_center / K_surround
         return W_cur
+
+    def get_K_img(self, center_p, size):
+        k_img = np.zeros((size, size))
+
+        if size % 2 != 1:
+            raise NameError('size must be odder')
+        filter_r = (size - 1) / 2
+        for cur_x in range(size):
+            cur_x = cur_x - filter_r
+            for cur_y in range(size):
+                cur_y = cur_y - filter_r
+                delta_p = np.array([cur_x, cur_y])
+
+                cur_p = center_p + delta_p
+                K = self.get_K(center_p, cur_p)
+                k_img[cur_x + filter_r][cur_y + filter_r] = K
+        return k_img
 
     def get_W_img(self,center_p,size):
         w_img = np.zeros((size,size))
@@ -106,6 +121,47 @@ class LSK_method:
                 W = self.get_W(center_p, cur_p)
                 w_img[cur_x + filter_r][cur_y + filter_r] = W
         return w_img
+
+    def getROI(self,center_p,size):
+        picblock = copyBlock_center(self.pic,center_p,size)
+        return picblock
+
+    def getDeriX(self,center_p,size):
+        picblock = copyBlock_center(self.pic_deri_x,center_p,size)
+        return picblock
+
+    def getDeriY(self,center_p,size):
+        picblock = copyBlock_center(self.pic_deri_y,center_p,size)
+        return picblock
+
+
+    def printInfo(self,center_p):
+        roi_x = center_p[0]
+        roi_y = center_p[1]
+        block_deri_x = copyBlock_center(self.pic_deri_x, (roi_x, roi_y), 5)
+        block_deri_y = copyBlock_center(self.pic_deri_y, (roi_x, roi_y), 5)
+        roixarr = mat2array(block_deri_x)
+        roiyarr = mat2array(block_deri_y)
+        mat_j = np.hstack((roixarr, roiyarr))
+        u, sigma, v = la.svd(mat_j)
+        s1 = sigma[0]
+        s2 = sigma[1]
+        v1 = np.matrix(v[:, 0])
+        v2 = np.matrix(v[:, 1])
+        print 'V mat:\n',v
+        print 'Sigma mat:\n',sigma
+        a1 = (s1 + 1) / (s2 + 1)
+        a2 = (s2 + 1) / (s1 + 1)
+        gamma = np.power((s1 + s2 + 1e-6) / 9, 0.008)
+        print 'gamma:',gamma,' a1_2:',a1,'  a2_2:',a2
+        print 'v1.T * v1\n',(a1 ** 2) * v1.T * v1
+        print 'v2.T * v2\n',(a2 ** 2) * v2.T * v2
+        mat_c = gamma * ((a1 ** 2) * v1.T * v1 + (a2 ** 2) * v2.T * v2)
+        print 'C mat:\n',mat_c
+        print 'det C:\n',la.det(mat_c)
+
+
+
 
 
 if __name__ == "__main__":
--- main_window.py
+++ main_window.py
@@ -7,6 +7,14 @@ from PIL import ImageTk
 from LSK_method import LSK_method
 import numpy as np
 
+
+def resizeByScale(img,scale):
+    shape_ori = img.shape
+    shape_new = [0, 0]
+    shape_new[0] = shape_ori[0] * scale
+    shape_new[1] = shape_ori[1] * scale
+    return cv2.resize(img, tuple(shape_new), interpolation=cv2.INTER_NEAREST)
+
 class mainWindow():
     def __init__(self):
         self.root = Tk()
@@ -18,32 +26,78 @@ class mainWindow():
         return ImageTk.PhotoImage(Image.fromarray(matimg))
 
     def callback(self,event):
-        print("Click position:", event.x, event.y)
-        clickx = event.x
-        clicky = event.y
-        clickp = np.array([event.x,event.y])
+        print("Click position:", event.x-1, event.y-1)
+
+        clickp = np.array([event.y-1,event.x-1])
         if self.tk_child == None:
             self.tk_child = Toplevel(self.root,borderwidth=5)
             self.tk_child.attributes("-toolwindow",True)
+            self.label0 = Label(master=self.tk_child,text= 'Weight Image')
+            self.label0.grid(row=0)
             self.subtk_label1 = Label(master=self.tk_child)
-            self.subtk_label1.pack()
+            self.subtk_label1.grid(row=1)
+
+            self.label2 = Label(master=self.tk_child,text= 'Gauss K Image').grid(row=2)
+            self.label3 = Label(master=self.tk_child)
+            self.label3.grid(row=3)
+
+            self.label4 = Label(master=self.tk_child,text= 'ROI').grid(row=4)
+            self.label5 = Label(master=self.tk_child)
+            self.label5.grid(row=5)
+
+            self.label6 = Label(master=self.tk_child,text='DeriX').grid(row=6)
+            self.label7 = Label(master=self.tk_child)
+            self.label7.grid(row=7)
+
+            self.label8 = Label(master=self.tk_child, text='DeriY').grid(row=8)
+            self.label9 = Label(master=self.tk_child)
+            self.label9.grid(row=9)
+
 
         w_img = self.lsk.get_W_img(clickp,11)
         np.set_printoptions(precision=2,suppress=True)
-        print w_img
+
+        k_img = self.lsk.get_K_img(clickp,11)
+        pic_roi = self.lsk.getROI(clickp,5)
+        pic_derix = self.lsk.getDeriX(clickp,5)
+        pic_deriy = self.lsk.getDeriY(clickp,5)
+        print 'K image:\n',k_img
+
+        print 'pic_roi\n',pic_roi
+        print 'pic_derix\n',pic_derix
+        print 'pic_deriy\n',pic_deriy
+
+
         w_img = cv2.normalize(w_img,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC1)
+        k_img = cv2.normalize(k_img,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC1)
 
-        fScale = 10
-        shape_ori = w_img.shape
-        shape_new = [0,0]
-        shape_new[0] = shape_ori[0] * fScale
-        shape_new[1] = shape_ori[1] * fScale
+        pic_derix = cv2.normalize(pic_derix,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC1)
+        pic_deriy = cv2.normalize(pic_deriy,alpha=0,beta=255,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_8UC1)
+
+        w_img_scale = resizeByScale(w_img,10)
+        k_img_scale = resizeByScale(k_img,10)
+        pic_roi_scale = resizeByScale(pic_roi,10)
+        pic_derix_scale = resizeByScale(pic_derix,10)
+        pic_deriy_scale = resizeByScale(pic_deriy,10)
 
 
-        w_img_scale = cv2.resize(w_img,tuple(shape_new),interpolation=cv2.INTER_NEAREST)
+
+
+
 
         self.weight_img = self.mat2tkimg(w_img_scale)
+        self.k_img = self.mat2tkimg(k_img_scale)
+        self.roi_img = self.mat2tkimg(pic_roi_scale)
+        self.derix_img = self.mat2tkimg(pic_derix_scale)
+        self.deriy_img = self.mat2tkimg(pic_deriy_scale)
+
         self.subtk_label1.configure(image=self.weight_img)
+        self.label3.configure(image=self.k_img)
+        self.label5.configure(image=self.roi_img)
+        self.label7.configure(image = self.derix_img)
+        self.label9.configure(image = self.deriy_img)
+
+        self.lsk.printInfo(clickp)
 
         self.tk_child.focus()
 
